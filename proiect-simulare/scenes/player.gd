65c167f3472ceb429bed54c4e036d872112ff18f
Dina Alexandru <alexdina692@gmail.com>
2024-04-09 18:53:31 +0300

    first steps for player movement and idle animation

--- proiect-simulare/scenes/player.gd
+++ proiect-simulare/scenes/player.gd
@@ -6,21 +6,18 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+@onready var animation = get_node("Sprite2D")
 
 func _physics_process(delta):
-	# Add the gravity.
+	animation.play("idle")
 	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		animation.play("move")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
